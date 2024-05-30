8e0a22ace8b2152d136cf2101b35de2cb77b6b4c
Marcos Rodriguez <mer.marcosr@gmail.com>
2024-05-30 18:10:00 -0300

    Add store persist

--- src/store/reduxStore.ts
+++ src/store/reduxStore.ts
@@ -4,19 +4,39 @@ import {moviesApi} from '../services/movies';
 import {authApi} from '../services/auth';
 import authReducer from './authSlice';
 import {usersApi} from '../services/user';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from 'redux-persist';
+
+const persistConfig = {
+  key: 'auth',
+  storage: AsyncStorage,
+  whitelist: ['isLogged', 'userId', 'moviePlayToken', 'refreshToken'],
+};
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
     [moviesApi.reducerPath]: moviesApi.reducer,
     [usersApi.reducerPath]: usersApi.reducer,
     [authApi.reducerPath]: authApi.reducer,
-    userSession: authReducer,
+    userSession: persistedAuthReducer,
   },
   middleware: getDefaultMiddleware => {
-    return getDefaultMiddleware().concat(
-      moviesApi.middleware,
-      usersApi.middleware,
-      authApi.middleware,
-    );
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(moviesApi.middleware, usersApi.middleware, authApi.middleware);
   },
 });
+
+export const persistor = persistStore(store);
--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import {StatusBar} from 'react-native';
+import {StatusBar, View} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import changeNavigationBarColor from 'react-native-navigation-bar-color';
 
@@ -9,10 +9,13 @@ import {Navigator} from './navigation/Navigator';
 import Toast from 'react-native-toast-message';
 import {toastConfig} from './components/commons/CustomToast';
 import {Provider as ReduxProvider} from 'react-redux';
-import {store} from '../store/reduxStore';
+import {persistor, store} from '../store/reduxStore';
 
 import {NoConnectionScreen} from './screens/noConnection/NoConnectionScreen';
 import {useNetInfo} from '@react-native-community/netinfo';
+import {PersistGate} from 'redux-persist/integration/react';
+import {LoadingModal} from './components/commons/modal/LoadingModal';
+import {COLORS} from '../constants/colors';
 
 function App(): React.JSX.Element {
   const netInfo = useNetInfo();
@@ -24,15 +27,29 @@ function App(): React.JSX.Element {
   return (
     <>
       <ReduxProvider store={store}>
-        <NavigationContainer>
-          <StatusBar translucent backgroundColor="transparent" />
-          {netInfo?.isConnected === null || netInfo?.isConnected ? (
-            <Navigator />
-          ) : (
-            <NoConnectionScreen />
-          )}
-        </NavigationContainer>
-        <Toast config={toastConfig} />
+        <PersistGate
+          loading={
+            <>
+              <LoadingModal isVisible />
+              <View
+                style={{
+                  backgroundColor: COLORS.BG,
+                  flex: 1,
+                }}
+              />
+            </>
+          }
+          persistor={persistor}>
+          <NavigationContainer>
+            <StatusBar translucent backgroundColor="transparent" />
+            {netInfo?.isConnected === null || netInfo?.isConnected ? (
+              <Navigator />
+            ) : (
+              <NoConnectionScreen />
+            )}
+          </NavigationContainer>
+          <Toast config={toastConfig} />
+        </PersistGate>
       </ReduxProvider>
     </>
   );
